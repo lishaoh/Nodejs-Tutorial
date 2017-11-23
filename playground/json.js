52c3efda2e0a7c66cd2fb5e40cd31642d2523eb1
lishaohua <lsh294753@gmail.com>
2017-11-23 17:16:47 +0800

    update

---
+++ playground/json.js
@@ -0,0 +1,28 @@
+// var obj = {
+//   name: 'Andrew'
+// };
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// var personString = '{"name": "Andrew", "age": 24}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some title',
+  body: 'Some body'
+};
+
+//originalNoteString
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+//note
+console.log(typeof note);
+console.log(note.title);
