5d4d6379319d57d1769ced35912ff6e0081d65e8
lishaohua <lsh294753@gmail.com>
2017-12-19 18:51:03 +0800

    testing asynchronous code

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -10,3 +10,28 @@ it('should square a number', () => {
   var res = utils.square(3);
   expect(res).toBe(9).toBeA('number');
 });
+
+// should verify first and last names are set
+// assert it includes firstName and lastName with proper values
+it('should set firstName and lastName', () => {
+  var  user = {location: 'Beijing', age: 23};
+  var res = utils.setName(user, "Andrew lsh");
+
+  expect(user).toInclude({
+    firstName: 'Andrew',
+    lastName: 'lsh'
+  })
+});
+
+// it('should expect some values', () => {
+//   // expect(12).toNotBe(12);
+//   // expect({name: 'Andrew'}).toNotEqual({name: 'Andrew'});
+//   // expect([2,3,4]).toExclude(1);
+//   expect({
+//     name: 'Andrew',
+//     age: 23,
+//     location: 'beijing'
+//   }).toExclude({
+//     age: 22
+//   })
+// });
