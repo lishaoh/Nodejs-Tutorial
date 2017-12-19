abc025677ce783041617917b4f97192ce29751b1
lishaohua <lsh294753@gmail.com>
2017-12-19 14:28:41 +0800

    remove uncode

--- web-server/server.js
+++ web-server/server.js
@@ -36,7 +36,6 @@ hbs.registerHelper('screamIt', (text) => {
 });
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express</h1>');
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website'
