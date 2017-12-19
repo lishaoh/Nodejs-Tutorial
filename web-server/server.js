d2a38cd07e47c5d2be85460830e1da4a2b91daea
lishaohua <lsh294753@gmail.com>
2017-12-19 11:58:43 +0800

    log

--- web-server/server.js
+++ web-server/server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
+  fs.appendFile('/web-server/server.log', log + '\n', (err) => {
     if (err) {
       console.log('Unable to append to server.log');
     }
