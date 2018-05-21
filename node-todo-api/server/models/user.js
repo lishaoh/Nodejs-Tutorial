981ad690a10fe967830b7ea4a419b692c95af3a1
lishaohua <lsh294753@gmail.com>
2018-05-21 15:45:56 +0800

    add post /todos route and refactor mongoose

---
+++ node-todo-api/server/models/user.js
@@ -0,0 +1,12 @@
+var {mongoose} = require('../db/mongoose');
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+module.exports = {User};
