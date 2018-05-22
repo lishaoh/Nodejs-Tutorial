be2b5678f3622963e2ded1b73b4501a3afa92513
lishaohua <lsh294753@gmail.com>
2018-05-22 16:16:48 +0800

    heroku

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -8,6 +8,8 @@ var {User} = require('./models/user');
 
 var app = express();
 
+const port = process.env.PORT || 9527;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -51,8 +53,8 @@ app.get('/todos/:id', (req, res) => {
       // 404 - and send empty body back
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 })
 
 module.exports = {app}
