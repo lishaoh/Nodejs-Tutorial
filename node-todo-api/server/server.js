187984db474eaad862ad29adf4b2979e05914ed5
lishaohua <lsh294753@gmail.com>
2018-05-22 14:43:30 +0800

    Add GET /todo/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -24,7 +25,31 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
-})
+});
+
+// GET /todos/123
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then(todo => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch(err => res.status(400).send());
+  // Valid id using inValid
+    // 404 - send back empty send
+
+  // findById
+    // success
+      // if todo - send it back
+      // if not todo - send back 404 with empty body
+    // error
+      // 404 - and send empty body back
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
