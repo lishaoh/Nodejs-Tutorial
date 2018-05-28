884f26272af230dcb8c5e90ca2034d4a43353ec2
lishaohua <lsh294753@gmail.com>
2018-05-28 14:16:50 +0800

    Add tests for DELETE /users/me/token

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -295,4 +295,23 @@ describe('POST /users/login', () => {
         }).catch(e => done(e));
       });
   });
+});
+
+describe('DELETE /users/me/token', () => {
+  it('should remove authorization token on logut', function(done) {
+    request(app)
+      .delete('/users/me/token')
+      .set('Authorization', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then(user => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch(e => done(e));
+      })
+  });
 })
