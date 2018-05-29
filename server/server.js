4de30a80c05a549a35f58e38a765c280683afe0d
lishaohua <lsh294753@gmail.com>
2018-05-29 19:16:38 +0800

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,8 +15,14 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.emit('newMessage', {
-    from: 'jack',
-    text: '嘿嘿嘿',
+    from: 'Admin',
+    text: 'New User',
+    createAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User joined',
     createAt: new Date().getTime()
   });
 
@@ -26,7 +32,12 @@ io.on('connection', (socket) => {
       from: message.from,
       text: message.text,
       createAt: new Date().getTime()
-    })
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
