0a859f26679851b3917fac4e26686694f1013d69
lishaohua <lsh294753@gmail.com>
2018-05-29 18:58:42 +0800

    Emit new Message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,11 @@ var socket = io();
 
 socket.on('connect', () => {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: "Andrew",
-    text: "中国共产党万岁"
-  });
 });
 
 socket.on('disconnect', () => {
   console.log('disconnected from server');
 });
-
 
 socket.on('newMessage', (message) => {
   console.log('newMessage', message);
